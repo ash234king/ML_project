2160fb17b15239d3905c4770e571516c981ffc2f
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-06-21 00:50:00 +0530

    model trainer

---
+++ notebook/model_training.py
@@ -0,0 +1,138 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.metrics import mean_squared_error,r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor,GradientBoostingRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression,Lasso,Ridge,ElasticNet
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
+from sklearn.model_selection import RandomizedSearchCV
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+import warnings
+
+df=pd.read_csv('notebook/data/stud.csv')
+print(df.head())
+
+x=df.drop(columns='math_score',axis=1)
+print(x.head())
+
+
+print("Categories in 'gender' variable:   ",end=" ")
+print(df['gender'].unique())
+
+print("Categories in 'race/ethnicity' variable: ",end=" ")
+print(df['race_ethnicity'].unique())
+
+print("Categories in 'parental level of education' variable:  ",end=" ")
+print(df['parental_level_of_education'].unique())
+
+print("Categories_in_lunch_variable: ",end=" ")
+print(df['lunch'].unique())
+
+print("Categories in 'test preparation course' variable:  ",end=" ")
+print(df['test_preparation_course'].unique())
+
+y=df['math_score']
+print(y)
+
+num_features=x.select_dtypes(exclude="object").columns
+cat_features=x.select_dtypes(include="object").columns
+
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.compose import ColumnTransformer
+
+numeric_transformer=StandardScaler()
+oh_transformer=OneHotEncoder()
+
+preprocessor=ColumnTransformer(
+    [
+        ("OneHotEncoder",oh_transformer,cat_features),
+        ("StandardScaler",numeric_transformer,num_features)
+    ]
+)
+
+x=preprocessor.fit_transform(x)
+
+print(x.shape)
+
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+print(x_train.shape,x_test.shape)
+
+def evaluate_model(true,predicted):
+    mse=mean_squared_error(true,predicted)
+    mae=mean_absolute_error(true,predicted)
+    rmse=np.sqrt(mse)
+    r2_square=r2_score(true,predicted)
+    return mae,rmse,r2_square
+
+models={
+    "Linear Regression":LinearRegression(),
+    "Lasso":Lasso(),
+    "Ridge":Ridge(),
+    "ElasticNet":ElasticNet(),
+    "K-Nearest Neighbours":KNeighborsRegressor(),
+    "Decision Tree":DecisionTreeRegressor(),
+    "Random Forest Regressor":RandomForestRegressor(),
+    "Adaboost Regressor":AdaBoostRegressor(),
+    "Gradient Boost regressor":GradientBoostingRegressor(),
+    "XGBRegressor":XGBRegressor(),
+    "CatBoosting Regressor":CatBoostRegressor(verbose=False),
+}
+
+model_list=[]
+r2_list=[]
+for i in range(len(list(models))):
+    model=list(models.values())[i]
+    model.fit(x_train,y_train)
+
+    y_train_pred=model.predict(x_train)
+    y_test_pred=model.predict(x_test)
+
+    model_train_mae,model_train_rmse,model__train_r2=evaluate_model(y_train,y_train_pred)
+    model_test_mae,model_test_rmse,model__test_r2=evaluate_model(y_test,y_test_pred)
+
+    print(list(models.keys())[i])
+
+    model_list.append(list(models.keys())[i])
+
+    print("Model performance for training dataset")
+    print("- Root Mean Squared error: {:.4f}".format(model_train_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_train_mae))
+    print("- R2 Score: {:.4f}".format(model__train_r2))
+
+    print('----------------------------------')
+    
+    print('Model performance for Test set')
+    print("- Root Mean Squared Error: {:.4f}".format(model_test_rmse))
+    print("- Mean Absolute Error: {:.4f}".format(model_test_mae))
+    print("- R2 Score: {:.4f}".format(model__test_r2))
+    r2_list.append(model__test_r2)
+    
+    print('='*35)
+    print('\n')   
+
+df2=pd.DataFrame(list(zip(model_list,r2_list)),columns=['Model Name','R2_score']).sort_values(by=["R2_score"],ascending=False)
+print(df2)
+
+lin_model=LinearRegression(fit_intercept=True)
+lin_model=lin_model.fit(x_train,y_train)
+y_pred=lin_model.predict(x_test)
+score=r2_score(y_test,y_pred)*100
+print("Accuracy of the model is %.2f" %score)
+
+plt.scatter(y_test,y_pred)
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
+plt.show()
+
+sns.regplot(x=y_test,y=y_pred,ci=None,color='red')
+plt.show()
+
+pred_df=pd.DataFrame({"Actual value":y_test,"Predicted Value":y_pred,"Difference":y_test-y_pred})
+print(pred_df)
