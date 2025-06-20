a54a5107dedaa075646ab9b8d7cd4a4e4ecb2b3f
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-06-20 07:34:03 +0530

    eda

--- notebook/EDA.py
+++ notebook/EDA.py
@@ -92,6 +92,7 @@ plt.show()
 
 ## Insights : Female students performance is better compared to male total wise also
 
+##on the basis of lunch type
 plt.subplots(1,3,figsize=(25,6))
 plt.subplot(141)
 sns.histplot(data=df,x='average',kde=True,hue='lunch')
@@ -102,3 +103,255 @@ sns.histplot(data=df[df.gender=='male'],x='average',kde=True,hue='lunch')
 plt.show()
 
 ##Insights: standard lunch helps student perform better in exams
+
+
+##On the basis of parental_level_of_education
+plt.subplots(1,3,figsize=(25,6))
+plt.subplot(141)
+sns.histplot(data=df,x='average',kde=True,hue='parental_level_of_education')
+plt.subplot(142)
+sns.histplot(data=df[df.gender=='female'],x='average',kde=True,hue='parental_level_of_education')
+plt.subplot(143)
+sns.histplot(data=df[df.gender=='male'],x='average',kde=True,hue='parental_level_of_education')
+plt.show()
+
+##Insights : for general graph the education is not having any effect on students average
+##           but for male distribution those having master's and associate degree have an effect on their student's average performance
+##           for female no such effect
+
+
+##on the basis of race
+plt.subplots(1,3,figsize=(25,6))
+plt.subplot(141)
+sns.histplot(data=df,x='average',kde=True,hue='race_ethnicity')
+plt.subplot(142)
+sns.histplot(data=df[df.gender=='female'],x='average',kde=True,hue='race_ethnicity')
+plt.subplot(143)
+sns.histplot(data=df[df.gender=='male'],x='average',kde=True,hue='race_ethnicity')
+plt.show()
+
+##insights : students of group A and B perform poorly in exam
+##           irrespective of their distribution by gender the A and B groups perform poorly
+
+
+plt.figure(figsize=(18,8))
+plt.subplot(1,4,1)
+plt.title('MATH SCORES')
+sns.violinplot(y='math_score',data=df,color='red',linewidth=3)
+plt.subplot(1,4,2)
+plt.title('READING SCORES')
+sns.violinplot(y='reading_score',data=df,color='green',linewidth=5)
+plt.subplot(1,4,3)
+plt.title('WRITING SCORE')
+sns.violinplot(y='writing_score',data=df,color='blue',linewidth=3)
+plt.show()
+
+##Insights:-
+## 1.for red one its slightly between 60-70 
+## 2.for blue one its between 70-75
+## 3.for green one its between 65-75
+
+
+plt.rcParams['figure.figsize']=(30,12)
+
+plt.subplot(1,5,1)
+size=df['gender'].value_counts()
+labels='Female','Male'
+color=['red','green']
+
+plt.pie(size,colors=color,labels=labels,autopct='.%2f%%')
+plt.title('Gender',fontsize=20)
+plt.axis('off')
+
+plt.subplot(1,5,2)
+size=df['race_ethnicity'].value_counts()
+labels='Group C','Group D','Group B','Group E','Group A'
+color=['red','green','blue','cyan','orange']
+
+plt.pie(size,colors=color,labels=labels,autopct='%.2f%%')
+plt.title('Race/Ethnicity',fontsize=20)
+plt.axis('off')
+
+plt.subplot(1,5,3)
+size=df['lunch'].value_counts()
+labels='Standard','Free'
+color=['red','green']
+
+plt.pie(size,colors=color,labels=labels,autopct='%.2f%%')
+plt.title('Lunch',fontsize=20)
+plt.axis('off')
+
+plt.subplot(1,5,4)
+size=df['test_preparation_course'].value_counts()
+labels='None','Completed'
+color=['red','green']
+
+plt.pie(size,colors=color,labels=labels,autopct='%.2f%%')
+plt.title('Test Course',fontsize=20)
+plt.axis('off')
+
+plt.subplot(1,5,5)
+size=df['parental_level_of_education'].value_counts()
+labels='Some College',"Associate's Degree",'High School','Some High School',"Bachelor's degree","Master's degree"
+color=['red','green','blue','cyan','orange','grey']
+
+plt.pie(size,colors=color,labels=labels,autopct='%.2f%%')
+plt.title('Parental Education',fontsize=20)
+plt.axis('off')
+
+plt.tight_layout()
+plt.grid()
+plt.show()
+
+##insights:
+## Number of male and female students are almost equal
+## Number of students are highest in group C
+## Number of student who are provided standard lunch are highest
+## Number of students who have not enrooled in any preparation course is greater
+## Number of students whose parental education is "Some college" is greater then "Associate's degree"
+
+
+
+f,ax=plt.subplots(1,2,figsize=(20,10))
+sns.countplot(x=df['gender'],data=df,palette='bright',ax=ax[0],saturation=0.95)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+
+plt.pie(x=df['gender'].value_counts(),labels=['Male','Female'],explode=[0,0.1],autopct='%1.1f%%',shadow=True,colors=['red','orange'])
+plt.show()
+
+## Gender has approximately balnced data with 51.8% male and 48.2% female
+
+
+
+gender_group=df.groupby('gender').mean(numeric_only=True)
+print(gender_group)
+
+plt.figure(figsize=(10,8))
+
+x=['Total Average','Math Average']
+
+female_scores=[gender_group['average'][0],gender_group['math_score'][0]]
+male_scores=[gender_group['average'][1],gender_group['math_score'][1]]
+
+x_axis=np.arange(len(x))
+
+plt.bar(x_axis-0.2,male_scores,0.4,label='Male')
+plt.bar(x_axis+0.2,female_scores,0.4,label='Female')
+
+plt.xticks(x_axis,x)
+plt.ylabel("Marks")
+plt.title("Total Marks v/s math average marks of both the genders",fontweight='bold')
+plt.legend()
+plt.show()
+
+## On an average females have a better overall score than men
+## But in maths males have scored higher than females
+
+f,ax=plt.subplots(1,2,figsize=(20,10))
+sns.countplot(x=df['race_ethnicity'],data=df,palette='bright',ax=ax[0],saturation=0.95)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+
+plt.pie(x=df['race_ethnicity'].value_counts(),explode=[0.1,0,0,0,0],autopct='%1.1f%%',shadow=True)
+plt.show()
+
+## most of the students are of group C/ group D
+## Lowest are from group A
+
+group_data2=df.groupby('race_ethnicity')
+f,ax=plt.subplots(1,3,figsize=(20,8))
+sns.barplot(x=group_data2['math_score'].mean().index,y=group_data2['math_score'].mean().values,palette='mako',ax=ax[0])
+ax[0].set_title('Math score',color='blue',size=20)
+
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=15)
+
+sns.barplot(x=group_data2['reading_score'].mean().index,y=group_data2['reading_score'].mean().values,palette='flare',ax=ax[1])
+ax[1].set_title('Reading Score',color='blue',size=20)
+
+for container in ax[1].containers:
+    ax[1].bar_label(container,color='black',size=15)
+
+sns.barplot(x=group_data2['writing_score'].mean().index,y=group_data2['writing_score'].mean().values,palette='flare',ax=ax[2])
+ax[2].set_title('Writing Score',color='blue',size=20)
+
+for container in ax[2].containers:
+    ax[2].bar_label(container,color='black',size=15)
+
+plt.show()
+
+## Group E has the highest marks in all three category
+## Group A has the lowest marks in all three category
+
+plt.rcParams['figure.figsize']=(15,9)
+plt.style.use('fivethirtyeight')
+sns.countplot(df['parental_level_of_education'],palette='Blues')
+plt.title('Comparison of parental education',fontweight=30,fontsize=20)
+plt.xlabel('Degree')
+plt.ylabel('count')
+plt.show()
+
+df.groupby('parental_level_of_education').mean(numeric_only=True).plot(kind='barh',figsize=(10,10))
+plt.legend(bbox_to_anchor=(1.05,1),loc=2,borderaxespad=0.)
+plt.show()
+
+##The score of student whose parent poses master and bachelors' degree is higher
+
+
+plt.rcParams['figure.figsize']=(15,9)
+plt.style.use('seaborn-talk')
+sns.countplot(df['lunch'],palette='PuBu')
+plt.title('Comparison of different types of lunch',fontweight=30,fontsize=20)
+plt.xlabel('types of lunch')
+plt.ylabel('count')
+plt.show()
+
+f,ax=plt.subplots(1,2,figsize=(20,8))
+sns.countplot(x=df['parental_level_of_education'],data=df,palette='bright',hue='test_preparation_course',saturation=0.95,ax=ax[0])
+ax[0].set_title('Students vs test preparation course ',color='black',size=25)
+for container in ax[0].containers:
+    ax[0].bar_label(container,color='black',size=20)
+
+sns.countplot(x=df['parental_level_of_education'],data=df,palette='bright',hue='lunch',saturation=0.95,ax=ax[1])
+for container in ax[1].containers:
+    ax[1].bar_label(container,color='black',size=20)
+plt.show()
+
+## students who get standard lunch tend to perform better than students who got free lunch
+
+plt.figure(figsize=(12,6))
+plt.subplot(2,2,1)
+sns.barplot(x=df['lunch'],y=df['math_score'],hue=df['test_preparation_course'])
+plt.subplot(2,2,2)
+sns.barplot(x=df['lunch'],y=df['reading_score'],hue=df['test_preparation_course'])
+plt.subplot(2,2,3)
+sns.barplot(x=df['lunch'],y=df['writing_score'],hue=df['test_preparation_course'])
+
+plt.show()
+
+## students who have completed the test preparation course have scores higher in all three categories
+
+
+##for checking outliers
+plt.subplots(1,4,figsize=(16,5))
+plt.subplot(141)
+sns.boxplot(df['math_score'],color='skyblue')
+plt.subplot(142)
+sns.boxplot(df['reading_score'],color='hotpink')
+plt.subplot(143)
+sns.boxplot(df['writing_score'],color='yellow')
+plt.subplot(144)
+sns.boxplot(df['average'],color='lightgreen')
+
+plt.show()
+
+
+sns.pairplot(df,hue='gender')
+plt.show()
+
+## Conclusion
+# student's performance is related with lunch, race, parental level education
+# females lead in pass percentage
+# students performance is not much related with test preparation course
+# finishing preparation course is beneficial
