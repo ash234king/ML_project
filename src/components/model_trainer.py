bdb353e5fe6c55a24e625316b9a78b24a788fc05
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-06-21 18:09:33 +0530

    hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -40,13 +40,69 @@ class ModelTrainer:
                 "XGBRegressor":XGBRegressor(),
                 "CatBoosting Regressor":CatBoostRegressor(),
             }
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
-                                             models=models)
+            params={
+                "Decision Tree":{
+                    'criterion' : ['squared_error','friedman_mse','absolute_error','poisson'],
+                    'splitter' : ['best','random'],
+                    'max_features': ['sqrt','log2']
+                },
+                "Linear Regression": {
+                    'fit_intercept':[True,False]
+                },
+                "Ridge":{
+                    'alpha': [0.1,0.2,0.01,1,0.03],
+                    'solver':['auto','svd','cholesky','lsqr','sparse_cg','sag','saga']
+                },
+                "Lasso":{
+                    'alpha': [0.1,0.2,0.01,1,0.03],
+                    'selection':['cyclic','random']
+                },
+                "ElasticNet":{
+                    'alpha': [0.1,0.2,0.01,1,0.03],
+                    'l1_ratio':[0.2,0.1,0.3,0.4,0.5],
+                    'selection':['random','cyclic']
+                },
+                "K-Nearest Neighbours":{
+                    'weights':['uniform','distance'],
+                    'algorithm':['auto','ball_tree','kd_tree','brute'],
+                    'metric':['minkowski']
+                },
+                "Random Forest Regressor":{
+                    'n_estimators':[10,20,40,50,100],
+                    'criterion':['squared_error','absolute_error','friedman_mse','poisson'],
+                    'max_features':['sqrt','log2'],
+                },
+                "Adaboost Regressor":{
+                    'learning_rate':[0.1,0.2,0.01,1,0.5],
+                    'loss':['linear','square','exponential'],
+                    'n_estimators':[2,8,32,64,88]
+                },
+                "Gradient Boost regressor":{
+                    'loss':['squared_error','absolute_error','huber','quantile'],
+                    'learning_rate':[0.1,0.2,0.01,1,0.5],
+                    'n_estimators':[2,8,32,64,88],
+                    'criterion':['friedman_mse', 'squared_error'],
+                    'max_features':['sqrt','log2']
+                },
+                "XGBRegressor":{
+                    'booster':['gbtree','gblinear','dart'],
+                    'n_estimators':[2,8,32,64,88],
+                    'learning_rate':[0.1,0.2,0.01,1,0.5]
+                },
+                "CatBoosting Regressor":{
+                    'learning_rate':[0.1,0.2,0.01,1,0.5],
+                    'loss_function':['RMSE','MAE','MAPE','Quantile'],
+                    'od_type':['IncToDec','Iter'],
+                    'boosting_type':['Ordered','Plain']
+                }
+            }
+            model_report,fitted_models=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,
+                                             models=models,param=params)
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
             ]
-            best_model=models[best_model_name]
+            best_model=fitted_models[best_model_name]
             if(best_model_score<0.6):
                 raise CustomException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import dill
 
 from src.exceptions import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import RandomizedSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -16,17 +17,30 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_models(x_train,y_train,x_test,y_test,models):
+def evaluate_models(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
+        fitted_models={}
         for i in range(len(list(models))):
             model=list(models.values())[i]
-            model.fit(x_train,y_train)
-            y_train_pred=model.predict(x_train)
-            y_test_pred=model.predict(x_test)
+            para=param[list(models.keys())[i]]
+
+            random=RandomizedSearchCV(model,para,cv=5,n_iter=15,n_jobs=-1,verbose=2,refit=True)
+            random.fit(x_train,y_train)
+            best_model=random.best_estimator_
+            y_train_pred=best_model.predict(x_train)
+            y_test_pred=best_model.predict(x_test)
             train_model_score=r2_score(y_train,y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]]=test_model_score
-        return report
+            fitted_models[list(models.keys())[i]]=best_model
+        return report,fitted_models
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
