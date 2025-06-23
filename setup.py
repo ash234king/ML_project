1a13d818e810774769d352da1eb0413ad5c7cb84
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-06-23 21:45:24 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,21 +1,4 @@
-from setuptools import find_packages,setup
-from typing import List
-
-def get_requirements(file_path:str)->List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-
-    HYPEN_E_DOT='-e.'
-    requirements=[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-        
-    return requirements
+from setuptools import find_packages, setup
 
 setup(
     name='ML_project',
@@ -23,5 +6,15 @@ setup(
     author='Yashvardhan Singh',
     author_email='yashvardhansingh9532@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=[
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'catboost',
+        'xgboost',
+        'scikit-learn',
+        'dill',
+        'flask'
+    ]
 )
